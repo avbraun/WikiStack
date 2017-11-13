a0c680cb1931212a7068ff610e51a13ff9841df5
Alexa Billings <alexa.billings@gmail.com>
2017-11-13 10:33:14 -0600

    commit with current Sequelize v. 4.22.6

--- index.js
+++ index.js
@@ -5,12 +5,19 @@ const nunjucks = require('nunjucks');
 const Sequelize = require('sequelize');
 const pg = require('pg');
 const pghstore = require('pg-hstore');
+const models = require('./models');
 
 var app = express();
 
 app.get('/', function(req, res){
   res.render('index');
 })
+
+// app.get('/wiki/', function(req, res){
+//   pages.findAll().then(function(pages){
+
+//   })
+// })
 
 nunjucks.configure('views', {noCache: true});
 
@@ -21,7 +28,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('public'))
 
-app.listen(3007, function(){
-  console.log('hello, I am the server');
+models.db.sync({force: true})
+.then(function(){
+  app.listen(3007, function(){
+    console.log('hello, I am the server');
+  });
 })
+.catch(function(err){console.error('failed on boot', err)});
+
+
 
--- models/index.js
+++ models/index.js
@@ -1,26 +1,54 @@
 var Sequelize = require('sequelize');
-var db = new Sequelize('postgres://localhost:3007/wikistack');
+var db = new Sequelize('postgres://localhost:5432/wikistack', {
+    logging: false
+});
 
 const Page = db.define('pages', {
   title: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false
   },
   urlTitle: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false,
+    get() {
+      const route = this.getDataValue('urlTitle');
+      return "/wiki/" + route
+    }
   },
   content: {
-    type: Sequelize.TEXT
+    type: Sequelize.TEXT,
+    allowNull: false
   },
-  status: {
-    type: Sequelize.ENUM('open', 'closed')
+  open: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+    allowNull: false
   },
-})
+  date: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW
+  }
+});
 
 const User = db.define('users', {
   name: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    isAlphanumeric: true,
+    allowNull: false
   },
   email: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    isEmail: true,
+    allowNull: false
   }
-})
+});
+
+
+module.exports = {
+  db: db,
+  Page: Page,
+  User: User
+};
+
+
